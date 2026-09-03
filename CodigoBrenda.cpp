0a39f338a0160eecb06c604412c084195f2643a5
faelabrenda-source <faelabrenda@gmail.com>
2026-09-03 20:44:11 +0100

    Update CodigoBrenda.cpp

--- CodigoBrenda.cpp
+++ CodigoBrenda.cpp
@@ -1,4 +1,42 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
 int main(){
 
-  return 0;
+    cout << "==============================\n";
+    cout << "=========JOGO DA FORCA========\n";
+    cout << "==============================\n";
+
+    string nomeJogador;
+
+    cout << "Digite o seu nome: ";
+    getline(cin, nomeJogador);
+
+    int opcao;
+
+    cout << "\n ------------ MENU ------------\n";
+    cout << "1 - Jogar\n";
+    cout << "2 - Ver ranking\n";
+    cout << "3 - Sair\n";
+
+    cout << "\nEscolha uma opção: ";
+    cin >> opcao;
+
+    if(opcao == 1){
+        cout << "\nVocê escolheu JOGAR!";
+    }
+    else if(opcao == 2){
+        cout << "\nO ranking será criado na próxima etapa.\n";
+    }
+    else if(opcao == 3){
+        cout << "\nAté a próxima, " << nomeJogador << "!\n";
+    }
+    else{
+        cout << "\nOpção inválida!\n";
+    }
+    
+
+    return 0;
 }
