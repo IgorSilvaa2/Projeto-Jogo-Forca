c8e0fe5b9e09cbf867a1b176bc56f3410e8bed1b
Igor Silva <boxigor13@gmail.com>
2026-09-08 11:11:32 +0100

    Refactor game functions and improve user input handling

--- Codigo_Igor.cpp
+++ Codigo_Igor.cpp
@@ -5,41 +5,120 @@
 
 using namespace std;
 
-void Nome_usuario(string a)
+/////////////////////// FUNCAO LIMPA TELA ///////////////////////////////////
+void limpa_Tela()
 {
-
+    system("CLS");
 }
-void Salva_nome(string a){
-    ofstream ficheiro("Nome_jogadores.txt", ios::app);
-    if(ficheiro.is_open()){
+/////////////////////// FUNCAO INPUT STRING ///////////////////////////////////
+void input_String(string a)
+{
+    ofstream ficheiro("Input_String.txt", ios::app);
+    if(ficheiro.is_open())
+    {
         ficheiro << a << "\n";
     }
     ficheiro.close();
 }
+////////////////////////// FUNCAO MENU ////////////////////////////////
+void menu_Inicial()
+{
+    string nome;
+    int escolha;
+
+    do
+    {
+        cout << "============================================================================================\n"
+     << "   _  ___   ____  ___    ____   _      _____ ___  ____   ____   _         +---+\n"
+     << "  | |/ _ \\ / ___|/ _ \\  |  _ \\ / \\    |  ___/ _ \\|  _ \\ / ___| / \\        |   |\n"
+     << "  | | | | | |  _| | | | | | | / _ \\   | |_ | | | | |_) | |    / _ \\       O   |\n"
+     << "  | | |_| | |_| | |_| | | |_/ / ___ \\  |  _|| |_| |  _ <| |___/ ___ \\    /|\\  |\n"
+     << " _/ |\\___/ \\____|\\___/  |____/_/   \\_\\ |_|   \\___/|_| \\_\\\\____/_/   \\_\\  / \\  |\n"
+     << "|__/                                                                          |\n"
+     << "============================================================================================\n" << endl;
+
+        cout << "Escolha uma opcao !";
+        cout << "\n 1 - Jogar";
+        cout << "\n 2 - Regras";
+        cout << "\n 3 - Sair";
+        cout << "\n =========================================\n";
+        cin >> escolha;
+
+        switch(escolha)
+        {
+        case 1 :
+            limpa_Tela();
+            cout << "=========================================================================\n"
+                 << "  ____  _____ __  __     __     ___ _   _ ____   ___  _ _ _ \n"
+                 << " | __ )| ____|  \\/  |    \\ \\   / / | \\ | |  _ \\ / _ \\| | | |\n"
+                 << " |  _ \\|  _| | |\\/| |     \\ \\ / /| |  \\| | | | | | | | | | |\n"
+                 << " | |_) | |___| |  | |      \\ V / | | |\\  | |_| | |_| |_|_|_|\n"
+                 << " |____/|_____|_|  |_|       \\_/  |_|_| \\_|____/ \\___/(_|_|_)\n"
+                 << "=========================================================================\n" << endl;
+
+            cout << "Digite o seu nome : ";
+            cin >> nome;
+            input_String(nome);
+            break;
+        case 2 :
+            limpa_Tela();
+            cout << "\n";
+            cout << "=========================================\n"
+                 << "         REGRA DO JOGO DA FORCA          \n"
+                 << "=========================================\n\n"
+                 << "1. OBJETIVO:\n"
+                 << "   Adivinhar a palavra secreta antes que o\n"
+                 << "   boneco seja totalmente desenhado.\n\n"
+                 << "2. COMO JOGAR:\n"
+                 << "   - Introduza uma letra de cada vez.\n"
+                 << "   - Se a letra existir, ela sera revelada.\n"
+                 << "   - Se errar, uma parte do corpo vai para a forca.\n\n"
+                 << "3. FIM DE JOGO:\n"
+                 << "   - VITORIA: Se descobrir todas as letras.\n"
+                 << "   - DERROTA: Se atingir o limite de 6 erros.\n\n"
+                 << "=========================================\n" << endl;
+            int sub_Escolha;
+
+            do
+            {
+                cout << "Escolha uma das opcoes. 1 - Voltar  2 - sair : ";
+                cin >> sub_Escolha;
+                if(sub_Escolha == 1)
+                {
+                    limpa_Tela();
+                    escolha = 0;
+                }
+                else if(sub_Escolha == 2)
+                {
+                    limpa_Tela();
+                    cout << "Ate logo !!";
+                    escolha = 3;
+                }
+                else
+                {
+                    cout << "Invalido " << endl;
+                    escolha = 0;
+                }
+            }
+            while (sub_Escolha < 1 || sub_Escolha > 2 );
+
+
+            break;
+        case 3 :
+            limpa_Tela();
+            cout << "Ate logo !!";
+            break;
+        }
+    }
+    while(escolha < 1 || escolha > 3);
+}
+//////////////////////// MAIN //////////////////////////////////
 
 int main()
 {
     setlocale(LC_ALL, "Portuguese");
-    string Nome;
-    int Escolha;
 
-    do{
-    cout << "***************************" << endl;
-    cout << "*      JOGO DA FORCA      *" << endl;
-    cout << "***************************" << endl;
-
-    cout << "digite o seu nome : ";
-    cin >> Nome;
-
-    Salva_nome(Nome);
-
-    cout << "Dezeja continuar ? 1 = Sim - 0 = Não : ";
-    cin >> Escolha;
-    }while(Escolha != 0);
-
-
-
-
+    menu_Inicial();
 
     return 0;
 }
